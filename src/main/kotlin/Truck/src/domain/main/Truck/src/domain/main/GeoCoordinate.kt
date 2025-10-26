d27d80e5d2643a4225fcafb5eab9f092ddd10cf7
evgeny <evgenv2015@gmail.com>
2025-10-26 15:58:24 +0500

    рефаторинг проверки валидных значений широты и долготы

--- src/main/kotlin/Truck/src/domain/main/Truck/src/domain/main/GeoCoordinate.kt
+++ src/main/kotlin/Truck/src/domain/main/Truck/src/domain/main/GeoCoordinate.kt
@@ -8,11 +8,11 @@ class GeoCoordinate(latitude1: Int, longitude1: Int) {
         private const val LOWER_LONGITUDE = -180
         private const val UPPER_LONGITUDE = 180
         fun from(latitude: Int, longitude: Int): GeoCoordinate {
-            if (latitude < LOWER_LATITUDE || latitude > UPPER_LATITUDE){
-                throw CreateGeoCoordinateError.LatitudeOutOfBound
-            }
-            if (longitude < LOWER_LONGITUDE || longitude > UPPER_LONGITUDE){
-                throw CreateGeoCoordinateError.LongitudeOutOfBound
+            when {
+                latitude !in LOWER_LATITUDE..UPPER_LATITUDE ->
+                    throw CreateGeoCoordinateError.LatitudeOutOfBound
+                longitude !in LOWER_LONGITUDE..UPPER_LONGITUDE ->
+                    throw CreateGeoCoordinateError.LongitudeOutOfBound
             }
             return GeoCoordinate(latitude, longitude)
         }
