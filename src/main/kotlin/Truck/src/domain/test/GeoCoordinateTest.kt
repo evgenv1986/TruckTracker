80e0c21f979a6702e353dedde9c93c08aae90a28
evgeny <evgenv2015@gmail.com>
2025-10-26 15:50:58 +0500

    перенес класс координат в отдельный файл

--- src/main/kotlin/Truck/src/domain/test/GeoCoordinateTest.kt
+++ src/main/kotlin/Truck/src/domain/test/GeoCoordinateTest.kt
@@ -1,5 +1,7 @@
 package org.example.Truck.src.domain.test
 
+import Truck.src.domain.main.Truck.src.domain.main.GeoCoordinate
+import Truck.src.domain.main.Truck.src.domain.main.CreateGeoCoordinateError
 import org.junit.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
@@ -33,26 +35,3 @@ class GeoCoordinateTest {
     }
 }
 
-class GeoCoordinate(latitude1: Int, longitude1: Int) {
-    companion object {
-        private const val LOWER_LATITUDE = -90
-        private const val UPPER_LATITUDE = 90
-
-        private const val LOWER_LONGITUDE = -180
-        private const val UPPER_LONGITUDE = 180
-        fun from(latitude: Int, longitude: Int): GeoCoordinate {
-            if (latitude < LOWER_LATITUDE || latitude > UPPER_LATITUDE){
-                throw CreateGeoCoordinateError.LatitudeOutOfBound
-            }
-            if (longitude < LOWER_LONGITUDE || longitude > UPPER_LONGITUDE){
-                throw CreateGeoCoordinateError.LongitudeOutOfBound
-            }
-            return GeoCoordinate(latitude, longitude)
-        }
-    }
-}
-
-sealed class CreateGeoCoordinateError (message: String) : Exception(message)  {
-    object LatitudeOutOfBound: CreateGeoCoordinateError("широта выходит за допустимые границы")
-    object LongitudeOutOfBound: CreateGeoCoordinateError("долгота выходит за допустимые границы")
-}
