c9171437ebe9cbf0bd5e0c2235276c1cc2d9a7bf
evgeny <evgenv2015@gmail.com>
2025-10-26 15:48:25 +0500

    Создание объекта координат с корректными значениями, с проверкой выхода за границы широты, долготы

--- src/main/kotlin/Truck/src/domain/test/GeoCoordinateTest.kt
+++ src/main/kotlin/Truck/src/domain/test/GeoCoordinateTest.kt
@@ -2,6 +2,7 @@ package org.example.Truck.src.domain.test
 
 import org.junit.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 class GeoCoordinateTest {
     @Test
@@ -17,13 +18,41 @@ class GeoCoordinateTest {
 
         val geoCoordinate = GeoCoordinate.from(latitude, longitude)
     }
+    @Test
+    fun `canT create with latitude out of bound`(){
+        assertEquals("широта выходит за допустимые границы",
+            assertFailsWith<CreateGeoCoordinateError.LatitudeOutOfBound> {
+                GeoCoordinate.from (-200, 10)}.message)
+    }
+
+    @Test
+    fun `canT create with longitude out of bound`(){
+        assertEquals("долгота выходит за допустимые границы",
+            assertFailsWith<CreateGeoCoordinateError.LongitudeOutOfBound> {
+                GeoCoordinate.from (20, 250)}.message)
+    }
 }
 
 class GeoCoordinate(latitude1: Int, longitude1: Int) {
     companion object {
+        private const val LOWER_LATITUDE = -90
+        private const val UPPER_LATITUDE = 90
+
+        private const val LOWER_LONGITUDE = -180
+        private const val UPPER_LONGITUDE = 180
         fun from(latitude: Int, longitude: Int): GeoCoordinate {
+            if (latitude < LOWER_LATITUDE || latitude > UPPER_LATITUDE){
+                throw CreateGeoCoordinateError.LatitudeOutOfBound
+            }
+            if (longitude < LOWER_LONGITUDE || longitude > UPPER_LONGITUDE){
+                throw CreateGeoCoordinateError.LongitudeOutOfBound
+            }
             return GeoCoordinate(latitude, longitude)
         }
     }
+}
 
+sealed class CreateGeoCoordinateError (message: String) : Exception(message)  {
+    object LatitudeOutOfBound: CreateGeoCoordinateError("широта выходит за допустимые границы")
+    object LongitudeOutOfBound: CreateGeoCoordinateError("долгота выходит за допустимые границы")
 }
